05d579145ac3802bbf508350cc9bc4b9c63d4160
pylypenko1 <pylypenko.romanio@gmail.com>
2023-04-24 14:42:36 +0300

    add the new folder email

---
+++ email/RU Rosatom [email] 2203/js/utm.js
@@ -0,0 +1,29 @@
+let objUTM = {
+  country: 'ru',
+  offer: 'Rosatom',
+  target: '',
+  creo: '',
+  buyer: ''
+};
+
+let currentUrl = window.location.href;
+
+if (currentUrl.indexOf('?') !== -1) {
+  let splitUrl = currentUrl.split('?');
+  let arrAttr = splitUrl[1].split('&');
+
+  for (let i = 0; i < arrAttr.length; i++) {
+
+    if (arrAttr[i].indexOf('targ') !== -1) {
+      objUTM.target = arrAttr[i].split('=')[1];
+    }
+
+    if (arrAttr[i].indexOf('creo') !== -1) {
+      objUTM.creo = arrAttr[i].split('=')[1];
+    }
+
+    if (arrAttr[i].indexOf('buyer') !== -1) {
+      objUTM.buyer = arrAttr[i].split('=')[1];
+    }
+  }
+}
